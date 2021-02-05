fcda460ff374b01b38422b26f7b0859a32f088aa
arielmell0 <arielmello@rede.ulbra.br>
2021-02-05 16:33:46 -0300

    alura sdk config :coffee:

--- src/main.kt
+++ src/main.kt
@@ -2,4 +2,11 @@ fun main(){
     println("Bem vindo ao bytebank!")
     val titular = "Ariel"
     println("Olá $titular, como você está hoje?")
+    val numeroConta = 5362
+    println("Número da conta: $numeroConta")
+    var saldoConta = 0
+    saldoConta = 100
+    saldoConta += 205
+    println("Saldo da sua conta: $saldoConta R$")
+
 }
