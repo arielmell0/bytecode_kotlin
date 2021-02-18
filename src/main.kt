ab50e8edf85275e5f59fc41d0da9ab8275ee0ce7
arielmell0 <arielmello@rede.ulbra.br>
2021-02-18 20:40:25 -0300

    Constructors and properties :pencil2:

--- src/main.kt
+++ src/main.kt
@@ -2,82 +2,77 @@ fun main() {
     println("Bem vindo ao Bytebank")
 
     //testaLacos()
-    var contaAriel = Conta()
-    contaAriel.titular = "Ariel"
-    contaAriel.numeroConta = 5321
-    contaAriel.setSaldo(150.0)
+    var contaAriel = Conta("Ariel", 1000)
+    contaAriel.deposita(150.0)
 
-    var contaCamily = Conta()
-    contaCamily.titular = "Camily"
-    contaCamily.numeroConta = 5920
-    contaCamily.setSaldo(-1500.0)
+    var contaCamily = Conta("Camily", 1001)
+    contaCamily.deposita(2000.0)
 
     println(contaAriel.titular)
     println(contaAriel.numeroConta)
-    println(contaAriel.getSaldo())
+    println(contaAriel.saldo)
 
     println(contaCamily.titular)
     println(contaCamily.numeroConta)
-    println(contaCamily.getSaldo())
+    println(contaCamily.saldo)
 
     println("Depositando na conta do Ariel")
     contaAriel.deposita(100.0)
-    println(contaAriel.getSaldo())
+    println(contaAriel.saldo)
     contaAriel.saca(500.0)
-    println("Seu novo saldo é: ${contaAriel.getSaldo()}")
+    println("Seu novo saldo é: ${contaAriel.saldo}")
 
     println("Depositando na conta da Camily")
     contaCamily.deposita(300.0)
-    println(contaCamily.getSaldo())
+    println(contaCamily.saldo)
 
     println("Transferindo da conta da Camily")
     contaCamily.trasferir(1000.0, contaAriel)
-    println("Seu saldo atual é ${contaCamily.getSaldo()}")
+    println("Seu saldo atual é ${contaCamily.saldo}")
 
 }
 
-class Conta {
-    var titular = ""
-    var numeroConta = 0
-    private var saldo = 0.0
+class Conta(
+    var titular: String,
+    var numeroConta: Int
+) {
+    var saldo = 0.0
+        private set
 
-    fun deposita(valor: Double){
-        this.saldo += valor
+
+//    construtor secundario
+//    constructor(titular: String, numeroConta: Int) {
+//        this.titular = titular
+//        this.numeroConta = numeroConta
+//    }
+
+    fun deposita(valor: Double) {
+        if(valor > 0) {
+            this.saldo += valor
+        }
     }
 
-    fun saca(valor: Double){
-        if(valor <= saldo){
+    fun saca(valor: Double) {
+        if (valor <= saldo) {
             saldo -= valor
         } else {
             println("Você não possui saldo o suficiente para essa transação.")
         }
     }
 
-    fun trasferir(valor: Double, destino: Conta): Boolean{
-        if(this.saldo >= valor){
+    fun trasferir(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
             saldo -= valor
             destino.saldo += valor
             return true
         }
         return false
     }
-
-    fun setSaldo(valor: Double){
-        if(valor > 0){
-            this.saldo = valor
-        }
-    }
-
-    fun getSaldo(): Double{
-        return this.saldo
-    }
-
-
 }
 
-fun testaLacos(){
+fun testaLacos() {
     var i = 0
-    while(i < 5){
+    while (i < 5) {
         val titular: String = "Ariel $i"
         val numeroConta: Int = 1000 + i
         var saldo = i + 10.0
